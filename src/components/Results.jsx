7c78314cb61dd8f44a5c43075fadea5606cecec1
vovaberdi <vova.berdibek@gmail.com>
2023-02-09 12:21:19 +0100

    create the card component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,11 @@
-import { Result } from 'postcss'
 import React from 'react'
+import Card from './Card'
 
 export default function Results({results}) {
   return (
-    <div>
+    <div className='sm:grid sm:grid-cols-2 lg:grid-clos-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
         {results.map((result)=> (
-            <div key={result.id}>
-                {result.original_title}
-            </div>
+            <Card key={result.id} result={result} />
         ))}
     </div>
   )
